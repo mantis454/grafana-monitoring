00a19af1fe276b80ed9845f58b7d7f68c42c0024
james <jc.tinsley@gmail.com>
2017-12-10 17:47:06 +1300

    updated disk temp

--- freenasmon.sh
+++ freenasmon.sh
@@ -26,51 +26,51 @@ do
         fi
         if [[ $line == *"WCC"* ]]
         then
-          da0_temp=$line
+          d_WDred=$line
         fi
-		if [[ $line == *"Z1D4RWRE"* ]]
+		if [[ $line == *"Z1D4RVYL"* ]]
         then
-          da1_temp=$line
+          d_Seag0=$line
         fi
-		if [[ $line == *"da2"* ]]
+		if [[ $line == *"W3015KFM"* ]]
         then
-          da2_temp=$line
+          d_Seag1=$line
         fi
-		if [[ $line == *"da3"* ]]
+		if [[ $line == *"W30164CV"* ]]
         then
-          da3_temp=$line
+          d_Seag2=$line
         fi
-		if [[ $line == *"da4"* ]]
+		if [[ $line == *"W3015KHJ"* ]]
         then
-          da4_temp=$line
+          d_Seag3=$line
         fi
-		if [[ $line == *"da5"* ]]
+		if [[ $line == *"W3015KBK"* ]]
         then
-          da5_temp=$line
+          d_Seag4=$line
         fi
-		if [[ $line == *"da6"* ]]
+		if [[ $line == *"WDH00WT4"* ]]
         then
-          da6_temp=$line
+          d_Seag5=$line
         fi
-		if [[ $line == *"da7"* ]]
+		if [[ $line == *"WDH00XG1"* ]]
         then
-          da7_temp=$line
+          d_Seag6=$line
         fi
-		if [[ $line == *"da8"* ]]
+		if [[ $line == *"WDH00XA7"* ]]
         then
-          da8_temp=$line
-        fi
-		if [[ $line == *"da9"* ]]
-        then
-          da9_temp=$line
+          d_Seag7=$line
         fi
 		if [[ $line == *"WDH00WM0"* ]]
         then
-          da10_temp=$line
+          d_Seag8=$line
         fi
 		if [[ $line == *"KINGSTON"* ]]
         then
-          ada0_temp=$line
+          d_king=$line
+        fi
+		if [[ $line == *"S2H7JD2B418146"* ]]
+        then
+          d_sams=$line
         fi
         #echo "... $line ..."
     done <<< "$hwinfo"
@@ -79,33 +79,46 @@ do
 	#clean this shit up
 	cpu0_temp=$(echo $cpu0_temp | cut -c24-25)
 	cpu1_temp=$(echo $cpu1_temp | cut -c24-25)
-	da0_temp=$(echo $da0_temp | cut -c5-6)
-	da1_temp=$(echo $da1_temp | cut -c5-6)
-	da2_temp=$(echo $da2_temp | cut -c5-6)
-	da3_temp=$(echo $da3_temp | cut -c5-6)
-	da4_temp=$(echo $da4_temp | cut -c5-6)
-	da5_temp=$(echo $da5_temp | cut -c5-6)
-	da6_temp=$(echo $da6_temp | cut -c5-6)
-	da7_temp=$(echo $da7_temp | cut -c5-6)
-	da8_temp=$(echo $da8_temp | cut -c5-6)
-	da9_temp=$(echo $da9_temp | cut -c5-6)
-	da10_temp=$(echo $da10_temp | cut -c6-7)
-	ada0_temp=$(echo $ada0_temp | cut -c6-7)
-	echo "$da1_temp"
+	
+	d_WDred_temp=$(echo $d_WDred | cut -d' ' -f2)
+	d_Seag0_temp=$(echo $d_Seag0 | cut -d' ' -f2)
+	d_Seag1_temp=$(echo $d_Seag1 | cut -d' ' -f2)
+	d_Seag2_temp=$(echo $d_Seag2 | cut -d' ' -f2)
+	d_Seag3_temp=$(echo $d_Seag3 | cut -d' ' -f2)
+	d_Seag4_temp=$(echo $d_Seag4 | cut -d' ' -f2)
+	d_Seag5_temp=$(echo $d_Seag5 | cut -d' ' -f2)
+	d_Seag6_temp=$(echo $d_Seag6 | cut -d' ' -f2)
+	d_Seag7_temp=$(echo $d_Seag7 | cut -d' ' -f2)
+	d_Seag8_temp=$(echo $d_Seag8 | cut -d' ' -f2)
+	d_king_temp=$(echo $d_king | cut -d' ' -f2)
+        d_sams_temp=$(echo $d_sams | cut -d' ' -f2)
+	
+	d_WDred_ser=$(echo $d_WDred | cut -d' ' -f3)
+	d_Seag0_ser=$(echo $d_Seag0 | cut -d' ' -f3)
+	d_Seag1_ser=$(echo $d_Seag1 | cut -d' ' -f3)
+	d_Seag2_ser=$(echo $d_Seag2 | cut -d' ' -f3)
+	d_Seag3_ser=$(echo $d_Seag3 | cut -d' ' -f3)
+	d_Seag4_ser=$(echo $d_Seag4 | cut -d' ' -f3)
+	d_Seag5_ser=$(echo $d_Seag5 | cut -d' ' -f3)
+	d_Seag6_ser=$(echo $d_Seag6 | cut -d' ' -f3)
+	d_Seag7_ser=$(echo $d_Seag7 | cut -d' ' -f3)
+	d_Seag8_ser=$(echo $d_Seag8 | cut -d' ' -f3)
+	d_king_ser=$(echo $d_king | cut -d' ' -f3)
+	d_sams_ser=$(echo $d_sams | cut -d' ' -f3)
 	
 	#Write this to the database
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=0 value=$da0_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=1 value=$da1_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=2 value=$da2_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=3 value=$da3_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=4 value=$da4_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=5 value=$da5_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=6 value=$da6_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=7 value=$da7_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=8 value=$da8_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=9 value=$da9_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=10 value=$da10_temp"
-	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=11 value=$ada0_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=0,serial=$d_WDred_ser value=$d_WDred_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=1,serial=$d_Seag0_ser value=$d_Seag0_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=2,serial=$d_Seag1_ser value=$d_Seag1_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=3,serial=$d_Seag2_ser value=$d_Seag2_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=4,serial=$d_Seag3_ser value=$d_Seag3_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=5,serial=$d_Seag4_ser value=$d_Seag4_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=6,serial=$d_Seag5_ser value=$d_Seag5_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=7,serial=$d_Seag6_ser value=$d_Seag6_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=8,serial=$d_Seag7_ser value=$d_Seag7_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=9,serial=$d_Seag8_ser value=$d_Seag8_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=10,serial=$d_king_ser value=$d_king_temp"
+	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=disk_temp,disk_number=11,serial=$d_sams_ser value=$d_sams_temp"
 	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=cpu_temp,cpu_number=0 value=$cpu0_temp"
 	curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "freenas_stats,host=freenas,type=cpu_temp,cpu_number=1 value=$cpu1_temp"	
 	
--- power.sh
+++ power.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-power1=$(ssh mfi@192.168.77.100 -C '/bin/cat /proc/analog/rms2')
-curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "power_stats,sensor=mains,type=current value=$power1"
-#echo $power
+ubntpower=$(ssh ubnt@192.168.77.100 -C '/bin/cat /proc/analog/rms1;/bin/cat /proc/analog/rms2')
+
+for var in line1 line2; do
+  IFS= read -r "$var" || break
+done <<< "$ubntpower"
+
+#echo $line1
+curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "power_stats,sensor=mains,type=current value=$line2"
+curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "power_stats,sensor=hwater,type=current value=$line1"
